e96cc5b7fceb2bc2c0aba6053889e148f5e9c58f
Damian <damianjava@gmail.com>
2019-04-23 23:05:33 +0200

    Primer commit

---
+++ projectUserSecurity/src/main/java/com/novellius/ProjectUserSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.novellius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUserSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUserSecurityApplication.class, args);
+	}
+
+}
